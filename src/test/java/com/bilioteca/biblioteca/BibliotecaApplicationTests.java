f4827687bf317a46b5bb3981edb5ac9af440981c
SeuDemar <seudemar@outlook.com>
2024-11-05 15:45:23 -0300

    testado vs code commit

---
+++ src/test/java/com/bilioteca/biblioteca/BibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bilioteca.biblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
