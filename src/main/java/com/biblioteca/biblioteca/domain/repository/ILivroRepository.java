77c7c9a997eaad8fa8dbd625065b150a443d9534
Demar2 <SeuDemar@outlook.com>
2024-11-14 18:51:02 -0300

    Import inutil

--- src/main/java/com/biblioteca/biblioteca/domain/repository/ILivroRepository.java
+++ src/main/java/com/biblioteca/biblioteca/domain/repository/ILivroRepository.java
@@ -3,8 +3,6 @@ package com.biblioteca.biblioteca.domain.repository;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.biblioteca.biblioteca.domain.entity.Livro;
-import java.util.List;
-
 
 public interface ILivroRepository extends JpaRepository<Livro, Long>{
     Optional<Livro> findByAnoPublicacao(int anoPublicacao);
